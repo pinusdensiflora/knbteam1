28c860fa61992a11488ee769d7bcbc28f35dfd5d
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-09-03 11:52:47 +0900

    day18

---
+++ Dawon/Dawon0903/finalexam/src/test/java/com/mysite/finalexam/FinalexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.finalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
