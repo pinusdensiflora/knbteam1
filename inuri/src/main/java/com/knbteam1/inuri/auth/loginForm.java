d4e2e2ab7ae0894315b163a55261fe92a3426423
kim-eunji0 <kkydu007@naver.com>
2024-09-10 17:39:43 +0900

    feat: loginForm
    로그인 폼 구현

---
+++ inuri/src/main/java/com/knbteam1/inuri/auth/loginForm.java
@@ -0,0 +1,20 @@
+/* 
+loginForm.java
+생성자: 김가은
+생성날짜: 9.10
+연락처: kkydu007@naver.com
+ */
+package com.knbteam1.inuri.auth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class loginForm {
+	@GetMapping("/loginForm")
+	
+	public String main() {
+		return "auth/loginForm";
+	}
+
+}
