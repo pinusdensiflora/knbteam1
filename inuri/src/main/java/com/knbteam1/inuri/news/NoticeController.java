c65b628be40e987a65aeb90cb031c66682081ba6
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-09-10 14:22:02 +0900

    feat notice.html

--- inuri/src/main/java/com/knbteam1/inuri/news/NoticeController.java
+++ inuri/src/main/java/com/knbteam1/inuri/news/NoticeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class NoticeController {
 	
 	
-	@GetMapping("/")
+	@GetMapping("")
 	public String notice() {
 		
 		return "news/notice";
