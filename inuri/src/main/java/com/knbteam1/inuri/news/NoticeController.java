b57bc3c227c28ee66067d2c08229344aa3c67766
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-09-10 15:42:52 +0900

    feat address, assist

--- inuri/src/main/java/com/knbteam1/inuri/news/NoticeController.java
+++ inuri/src/main/java/com/knbteam1/inuri/news/NoticeController.java
@@ -8,12 +8,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class NoticeController {
 	
-	
+	//공지사항탭
 	@GetMapping("")
 	public String notice() {
 		
 		return "news/notice";
 		
 	}
+	
+	//소식탭
+	@GetMapping("/info")
+	public String companyNews() {
+		
+		return "news/info/companyNews";
+		
+	}
+	
+	
+	//고객지원탭
+	@GetMapping("/assist")
+	public String assist() {
+		return "news/assist/FAQ";
+	}
+	
+	@GetMapping("/assist/inquiry")
+	public String inquiry() {
+		return "news/assist/inquiry";
+	}
+	
+	
+	
+	
+	//찾아오시는 길
+	@GetMapping("/address")
+	public String address() {
+		return "news/address";
+		
+	}
+	
 
 }
