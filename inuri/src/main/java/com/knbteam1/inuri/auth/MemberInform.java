5fa3b9311347d8f7dc44e32367dd3f58eab4dbb8
KKAEUN <kkydu007@naver.com>
2024-09-11 11:39:18 +0900

    feat:회원가입 화면 구현

--- inuri/src/main/java/com/knbteam1/inuri/auth/MemberInform.java
+++ inuri/src/main/java/com/knbteam1/inuri/auth/MemberInform.java
@@ -1,20 +1,28 @@
 /* 
-loginForm.java
+MemberInform.java
 생성자: 김가은
-생성날짜: 9.10
+생성날짜: 9.11
 연락처: kkydu007@naver.com
  */
+
+
 package com.knbteam1.inuri.auth;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class loginForm {
-	@GetMapping("/loginForm")
+public class MemberInform {
+	@GetMapping("/memberInform")
 	
 	public String main() {
-		return "auth/loginForm";
+		return "auth/memberInform";
 	}
 
 }
+
+
+
+
+
+
