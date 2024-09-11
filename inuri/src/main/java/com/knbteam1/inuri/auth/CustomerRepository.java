2de7440f3c4cb29e719e203ae78a060f9b2bba3e
KKAEUN <kkydu007@naver.com>
2024-09-11 17:34:30 +0900

    refactor : login

--- inuri/src/main/java/com/knbteam1/inuri/auth/CustomerRepository.java
+++ inuri/src/main/java/com/knbteam1/inuri/auth/CustomerRepository.java
@@ -1,10 +1,9 @@
-/*
- * package com.knbteam1.inuri.auth;
- * 
- * import java.util.Optional; import
- * org.springframework.data.jpa.repository.JpaRepository;
- * 
- * public interface CustomerRepository extends JpaRepository<Customer, Integer>
- * { //스프링 시큐리티 로그인 아이디로 객체 찾기 Optional<Customer> findByusername(String
- * username); }
- */
+package com.knbteam1.inuri.auth;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+	//스프링 시큐리티 로그인 아이디로 객체 찾기
+		Optional<Customer> findByusername(String username);
+}
