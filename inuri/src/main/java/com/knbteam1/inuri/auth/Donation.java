366656361031d00db30338f65a61415948267c85
KKAEUN <kkydu007@naver.com>
2024-09-12 15:27:45 +0900

    feat : Donation 엔티티 추가

---
+++ inuri/src/main/java/com/knbteam1/inuri/auth/Donation.java
@@ -0,0 +1,38 @@
+/* 
+Donation.java
+생성자: 김가은
+생성날짜: 9.12
+연락처: kkydu007@naver.com
+ */
+
+
+
+package com.knbteam1.inuri.auth;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Donation {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer did;
+	private LocalDateTime ddate;
+	private Integer dwon;
+	private String dwhere;
+	
+	@ManyToOne
+	
+	private Customer customer;
+	
+
+
+}
