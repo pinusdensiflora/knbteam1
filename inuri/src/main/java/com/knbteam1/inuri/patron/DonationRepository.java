a157f2e29ac036663dc5250be92cd0fe579f5914
ghwan07 <ghwan07@gmail.com>
2024-09-25 13:29:53 +0900

    feat donationrepository

--- inuri/src/main/java/com/knbteam1/inuri/patron/DonationRepository.java
+++ inuri/src/main/java/com/knbteam1/inuri/patron/DonationRepository.java
@@ -1,4 +1,3 @@
-
 /*
  * 생성자 : 김근환 
  * 생성일 : 9.15 
@@ -10,18 +9,5 @@ package com.knbteam1.inuri.patron;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DonationRepository extends JpaRepository<Donation, Integer> {
-
-
-package com.knbteam1.inuri.patron;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.time.LocalDateTime;
-
-public interface DonationRepository extends JpaRepository<Donation, Integer> {
-    @Query("SELECT SUM(d.donationAmount) FROM Donation d WHERE d.ddate = :currentDate")
-    Integer findTotalDonationsByDate(@Param("currentDate") LocalDateTime currentDate);
 
 }
