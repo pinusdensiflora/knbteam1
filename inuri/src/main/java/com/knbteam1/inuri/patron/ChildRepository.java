3884e95fc510b2fc9ac72bf115380d5569a5f254
Hwan <ghwan07@gmail.com>
2024-09-25 11:35:29 +0900

    Revert "update"
    This reverts commit 1ebc8054dd21d7605e03bd6d36bd8ce9ba978756.

--- inuri/src/main/java/com/knbteam1/inuri/patron/ChildRepository.java
+++ inuri/src/main/java/com/knbteam1/inuri/patron/ChildRepository.java
@@ -1,19 +1,16 @@
 /*
- * 생성자 : 김근환 
- * 생성일 : 9.13 
- * 연락처 : ghwan07@gmail.com
+ * 생성자 : 김근환 생성일 : 9.13 연락처 : ghwan07@gmail.com
  */
 package com.knbteam1.inuri.patron;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChildRepository extends JpaRepository<Child, Integer> {
-	 //페이징
+
 	 Page<Child> findAll(Pageable pageable);
-	 
-	 //검색
-	 Page<Child> findAll(Specification<Child> spec, Pageable pageable);
+	 List<Child> findByChnameContaining(String keyword);
 }
