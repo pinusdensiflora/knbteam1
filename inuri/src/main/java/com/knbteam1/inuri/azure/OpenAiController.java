4173a979344e97705f09798dd6ea95cdd87f5748
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-09-29 19:08:21 +0900

    Feat: WebConfig

--- inuri/src/main/java/com/knbteam1/inuri/azure/OpenAiController.java
+++ inuri/src/main/java/com/knbteam1/inuri/azure/OpenAiController.java
@@ -1,9 +1,13 @@
 package com.knbteam1.inuri.azure;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/images")
@@ -15,10 +19,15 @@ public class OpenAiController {
     }
 
     @PostMapping("/generate")
-    public String generateImage(@RequestBody String prompt) {
-        System.out.println("prompt = " + prompt);
-        String response =openAiService.generateImage(prompt);
+    public ResponseEntity<Map<String, String>> generateImage(@RequestBody String imagePrompt) {
+        System.out.println("imagePrompt = " + imagePrompt);
+        String imageUrl = openAiService.generateImage(imagePrompt);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("imageUrl", imageUrl);
+
         System.out.println("response = " + response);
-        return response;
+
+        return ResponseEntity.ok(response);
     }
 }
